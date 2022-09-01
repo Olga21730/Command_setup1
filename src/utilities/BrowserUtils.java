dde14bc4b0533442a1225cb01a3b58cb2a725143
Olga <buruiana.olea@gmail.com>
2022-08-31 21:00:30 -0500

    create BrowserUtils

---
+++ src/utilities/BrowserUtils.java
@@ -0,0 +1,9 @@
+package utilities;
+
+public class BrowserUtils {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        System.out.println("add info");
+    }
+}
